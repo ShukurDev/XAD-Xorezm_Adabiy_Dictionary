beb4d9d50a450889b0bbeaea6a9bd9a3017ac1db
Shukurali <81503330+ShukurDev@users.noreply.github.com>
2021-09-27 22:01:58 +0500

    db

--- dict/models.py
+++ dict/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Lugat(models.Model):
-    adabiy = models.CharField('Adabiy (soz)' , max_length=100)
-    xorazmcha = models.CharField('Xorazm (soz)' ,max_length=100)
+    adabiy = models.CharField('Adabiy (soz)' , max_length=100 , null = True , blank = True)
+    xorazmcha = models.CharField('Xorazm (soz)' ,max_length=100 ,null = True , blank = True)
 
